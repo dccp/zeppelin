df5b0e507f98fae5abbe03937ee84baa08c52e1f
Wilhelm Hedman <hedmanw@student.chalmers.se>
2015-03-04 15:45:55 +0100

    Begun work on local test environment

---
+++ js/zeppelin.js
@@ -0,0 +1,9 @@
+
+$( "#deploy-github" ).click(function() {
+    alert( "Do some stuff from Github" );
+});
+
+$( "#deploy-local" ).click(function() {
+    alert( "Do some stuff with local file storage" );
+});
+
