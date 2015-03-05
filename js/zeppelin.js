3e0a006f225371685d2647e8d3eb46686be4370a
Wilhelm Hedman <hedmanw@student.chalmers.se>
2015-03-05 13:50:45 +0100

    Added blockchain metainfo to index

--- js/zeppelin.js
+++ js/zeppelin.js
@@ -1,9 +1,21 @@
 
-$( "#deploy-github" ).click(function() {
-    alert( "Do some stuff from Github" );
-});
-
 $( "#deploy-local" ).click(function() {
     alert( "Do some stuff with local file storage" );
+});
+
+web3.eth.watch('chain').changed(function(){
+  var coinbase = web3.eth.coinbase;
+  document.getElementById('coinbase').innerText = coinbase;
+  var balance = web3.eth.balanceAt(coinbase);
+  document.getElementById('balance').innerText = web3.toDecimal(balance);
+});
+
+web3.eth.watch('pending').changed(function(){
+  var blockNumber = web3.eth.number;
+  document.getElementById('latestBlock').innerText = blockNumber;
+  var hash = web3.eth.block(blockNumber).hash;
+  document.getElementById('latestBlockHash').innerText = hash;
+  var timeStamp = web3.eth.block(blockNumber).timestamp;
+  document.getElementById('latestBlockTimestamp').innerText = Date(timeStamp);
 });
 
