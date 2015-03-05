fed5f6227803013e9d0c90508696e7b9b6310baa
Oskar Nyberg <oskar@oskarnyberg.com>
2015-03-05 14:53:59 +0100

    Imported json and renamed function

--- python/ethereum.py
+++ python/ethereum.py
@@ -1,18 +1,18 @@
-import ethereumJSON
+import json, ethereumJSON
 
 class EthereumAPI:
 
     def __init__(self):
         self.json = ethereumJSON.EthereumJSON()
 
-    def transact(self, code):
-        return self.json.sendJSONRequest("eth_transact", json.dumps({"code":code}))
+    def sendTransaction(self, code):
+        return self.json.sendJSONRequest("eth_sendTransaction", json.dumps({"code":code}))
 
     # TODO: change to eth_compileSolidity
     def compileSolidity(self, contract):
         return self.json.sendJSONRequest("eth_solidity", contract)
 
 eth = EthereumAPI()
-source = open("WorkerDispatcher.sol", "r").read()
+source = open("contracts/WorkerDispatcher.sol", "r").read()
 compiled = eth.compileSolidity(source)
-print eth.transact(compiled)
+print eth.sendTransaction(compiled)
