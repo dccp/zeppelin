476445b68daf483bd7f1aafcb3b7971bf5daea75
Wilhelm Hedman <hedmanw@student.chalmers.se>
2015-03-08 23:06:10 +0100

    Zeppelin works in external browsers

--- js/zeppelin.js
+++ js/zeppelin.js
@@ -1,24 +1,22 @@
-$( "#deploy-local" ).click(function() {
-    alert( "Do some stuff with local file storage" );
-});
+var web3 = require('web3');
+web3.setProvider(new web3.providers.HttpSyncProvider());
 
 web3.eth.watch('chain').changed(function(){
-  var coinbase = web3.eth.coinbase;
-  document.getElementById('coinbase').innerText = coinbase;
-  var balance = web3.eth.balanceAt(coinbase);
-  document.getElementById('balance').innerText = web3.toDecimal(balance);
+    var coinbase = web3.eth.coinbase;
+    $('#coinbase').text(coinbase);
+    var balance = web3.eth.balanceAt(coinbase);
+    $('#balance').text(web3.toDecimal(balance));
 });
 
 web3.eth.watch('pending').changed(function(){
-  var blockNumber = web3.eth.number;
-  document.getElementById('latestBlock').innerText = blockNumber;
-  var hash = web3.eth.block(blockNumber).hash;
-  document.getElementById('latestBlockHash').innerText = hash;
-  var timeStamp = web3.eth.block(blockNumber).timestamp;
-  document.getElementById('latestBlockTimestamp').innerText = Date(timeStamp);
-  document.getElementById('contractAddress').innerText = contractAddress;
+    var blockNumber = web3.eth.number;
+    $('#latestBlock').text(blockNumber);
+    var hash = web3.eth.block(blockNumber).hash;
+    $('#latestBlockHash').text(hash);
+    var timeStamp = web3.eth.block(blockNumber).timestamp;
+    $('#latestBlockTimestamp').text(Date(timeStamp));
+    $('#contractAddress').text(contractAddress);
 
-  var contractString = JSON.stringify(web3.eth.storageAt(contractAddress));
-  document.getElementById('contractStorage').innerText = contractString;
+    var contractString = JSON.stringify(web3.eth.storageAt(contractAddress));
+    $('#contractStorage').text(contractString);
 });
-
