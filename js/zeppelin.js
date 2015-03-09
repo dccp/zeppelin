5ecd0598b547334cff157d8b3b598a59d443f481
Oskar Nyberg <oskar@oskarnyberg.com>
2015-03-09 14:35:32 +0100

    Added accounts info and refactored

--- js/zeppelin.js
+++ js/zeppelin.js
@@ -1,9 +1,14 @@
 var web3 = require('web3');
 web3.setProvider(new web3.providers.HttpSyncProvider());
 
+var workerDispatcher = web3.eth.contract(contractAddress, contractStructure);
+var numWorkers = workerDispatcher.numWorkers();
+
 web3.eth.watch('chain').changed(function(){
     var coinbase = web3.eth.coinbase;
     $('#coinbase').text(coinbase);
+    var accounts = web3.eth.accounts;
+    $('#accounts').text(accounts);
     var balance = web3.eth.balanceAt(coinbase);
     $('#balance').text(web3.toDecimal(balance));
 });
@@ -20,15 +25,13 @@ web3.eth.watch('pending').changed(function(){
     var contractString = JSON.stringify(web3.eth.storageAt(contractAddress));
     $('#contractStorage').text(contractString);
 
-    var workerDispatcher = web3.eth.contract(contractAddress, contractStructure);
-    var numWorkers = workerDispatcher.numWorkers();
     $('#numWorkers').text(numWorkers);
+});
 
-    $('#workerId').bind('input', function() {
-        var id = $('#workerId').val();
-        console.log(id);
-        var info = workerDispatcher.workersInfo(workerDispatcher.workerList(id));
-        console.log(info);
-        $('#workerInfo').text(info);
-    });
+$('#workerId').bind('input', function() {
+    var id = $('#workerId').val();
+    console.log(id);
+    var info = workerDispatcher.workersInfo(workerDispatcher.workerList(id));
+    console.log(info);
+    $('#workerInfo').text(info);
 });
