d4fe37895db14c65d826b5ef1ebba7c20337154d
Wilhelm Hedman <hedmanw@student.chalmers.se>
2015-04-02 11:43:20 +0200

    Added Zep REST endpoint in backend

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 // Serve static files
 app.use(express.static(__dirname + '/build'));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/images', function(req, res) {
+    res.json({test: "hej"});
+});
+
+app.post('/transfer', function(req, res) {
+    var imageHash = req.body.imageHash;
+    var host = req.body.host;
+    var port = req.body.port;
+    res.send(imageHash + " " + host + ":" + port + "\n");
+});
 
 // Run server
 var server = app.listen(8000, function () {
--- src/components/ClientPanel.jsx
+++ src/components/ClientPanel.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import EthClient from "../client/ethclient.js";
+import jquery from "jquery";
 
 let ClientPanel = React.createClass({
     getInitialState() {
         return ({
             workers: [],
             minLength: 0,
-            maxPrice: Number.POSITIVE_INFINITY
+            maxPrice: Number.POSITIVE_INFINITY,
+            images: "No Docker images..."
         });
     },
 
     componentDidMount() {
         this.repopulateWorkerList(null, null);
+        jquery.ajax({
+          url: "/images",
+          dataType: 'json',
+          success: function(data) {
+            this.setState({images: data});
+          }.bind(this),
+          error: function(xhr, status, err) {
+            console.error(document.URL, status, err.toString());
+          }.bind(this)
+        });
     },
 
     repopulateWorkerList(length, price) {
@@ -33,6 +45,17 @@ let ClientPanel = React.createClass({
     submit(pubkey, price) {
         let length = this.refs.minLength.getDOMNode().value;
         EthClient.buyContract(pubkey, 1, price, length);
+        jquery.ajax({
+          url: "/transfer",
+          type: 'POST',
+          data: {"imageHash":"someHash", "host":"someHost", "port":"somePort"},
+          success: function(data) {
+            console.log("Sent docker-xfer:" + data);
+          }.bind(this),
+          error: function(xhr, status, err) {
+            console.error(document.URL, status, err.toString());
+          }.bind(this)
+        });
     },
 
     render() {
@@ -47,6 +70,7 @@ let ClientPanel = React.createClass({
                     <h1>Client frontend. Deal with it.</h1>
                     <div className="row">
                         <div className="col-md-4">
+                            <p>{this.state.images}</p>
                             <div className="form-group">
                                 <label>Minimum length</label>
                                 <input className="form-control" onChange={this.changeMinLength} value={this.state.minLength} type="number" placeholder="Min length" ref="minLength" />
