450e8ac2a270b94b5b9e6c655c1aec509e841ebb
Johan Lindskogen <johan.lindskogen@gmail.com>
2015-04-15 14:00:00 +0200

    Submit form onchange, disable button if empty input

--- src/components/JsonRPC.jsx
+++ src/components/JsonRPC.jsx
@@ -5,8 +5,13 @@ import Dashboard from "./Dashboard.jsx";
 let JsonRPC = React.createClass({
     getInitialState() {
         return {
-            json_rpc_url: EthClient.getJsonRPCUrl()
+            json_rpc_url: EthClient.getJsonRPCUrl(),
+            submittable: false
         };
+    },
+    enableButton() {
+        let val = this.refs.jsonRpcInput.getDOMNode().value.trim();
+        this.setState({submittable: val.length != 0});
     },
     handleJsonRpcSubmit(e) {
         e.preventDefault();
@@ -29,18 +34,18 @@ let JsonRPC = React.createClass({
                         <div>
                             <div className="form-group">
                                 <label labelFor="jsonrpc">JSON RPC URL</label>
-                                <input type="url" className="form-control" id="jsonrpc" ref="jsonRpcInput" placeholder={this.state.json_rpc_url}/>
+                                <input type="url" className="form-control" id="jsonrpc" ref="jsonRpcInput" onInput={this.enableButton} placeholder={this.state.json_rpc_url}/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="jsonrpc_predefined">Predefined RPC URL</label>
-                                  <select className="form-control" id="jsonrpc_predefined" defaultValue={this.state.json_rpc_url} ref="jsonRpcInput_predefined">
+                                  <select className="form-control" id="jsonrpc_predefined" onChange={this.handleJsonRpcSubmit} defaultValue={this.state.json_rpc_url} ref="jsonRpcInput_predefined">
                                     <option value="http://localhost:8080">localhost:8080</option>
                                     <option value="http://qng.se:4041">qng.se:4041</option>
                                     <option value="http://qng.se:4042">qng.se:4042</option>
                                     <option value="http://qng.se:4043">qng.se:4043</option>
                                   </select>
                             </div>
-                            <button type="submit" className="btn btn-primary">Change</button>
+                            <button type="submit" className="btn btn-primary" disabled={!this.state.submittable}>Change</button>
                         </div>
                     </form>
                 </div>
